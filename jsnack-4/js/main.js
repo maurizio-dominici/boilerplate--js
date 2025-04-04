d8cf0a5f954b267f4a5bbc82cdd2db8bc1f07bff
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-04 11:53:28 +0200

    modified jsnack

--- jsnack-4/js/main.js
+++ jsnack-4/js/main.js
@@ -1,12 +1,10 @@
 const emptyArray = [];
 
-let outputText = "";
-
-for (let i = 0; i <= 6; i++) {
-  const answerUser = Number(prompt("inserisci un numero"));
-  if (answerUser / 2 === 1) {
-    outputText = emptyArray[answerUser];
+for (let i = 0; i < 6; i++) {
+  const answerUser = parseInt(prompt("inserisci un numero"));
+  if (answerUser % 2 === 1) {
+    emptyArray.push(answerUser);
   }
 }
 
-console.log(outputText);
+console.table(emptyArray);
