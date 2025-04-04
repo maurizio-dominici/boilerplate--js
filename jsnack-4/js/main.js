b4b1ef33bf37e7e1c157bd67432096b1e3968d59
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-04 13:11:41 +0200

    jack 7

--- jsnack-4/js/main.js
+++ jsnack-4/js/main.js
@@ -1,3 +1,4 @@
+// ^jack-4
 const emptyArray = [];
 
 for (let i = 0; i < 6; i++) {
@@ -8,3 +9,27 @@ for (let i = 0; i < 6; i++) {
 }
 
 console.table(emptyArray);
+
+//^ jack 5
+const n = prompt("di quanti numeri vuoi il cubo");
+
+for (let i = 0; i < n; i++) {
+  console.log(i * i * i);
+}
+
+// ^jack 7
+
+const fourNuber = prompt("massimo 4 cifre");
+
+let sumNumb = 0;
+// console.log(fourNuber);
+if (fourNuber.length <= 4) {
+  for (let i = 0; i < fourNuber.length; i++) {
+    const currentNumber = parseInt(fourNuber[i]);
+    console.log("currentNumber", currentNumber);
+    sumNumb += currentNumber;
+  }
+  console.log(alert(sumNumb));
+} else {
+  console.log(alert("troppe cifre"));
+}
